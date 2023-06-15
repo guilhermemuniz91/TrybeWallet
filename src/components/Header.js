ecda8f336f94bde251dc39f027cba131e0306c27
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-06-15 20:22:52 -0300

    req2 feito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  expensesSum = () => {
+    const { expenses } = this.props;
+    let sum = 0;
+
+    expenses.forEach((oneExpense) => {
+      const { value, currency, exchangeRates } = oneExpense;
+      sum += value * exchangeRates[currency].ask;
+    });
+
+    return parseFloat(sum).toFixed(2);
+  };
+
   render() {
+    const { email } = this.props;
+    const totalExpenses = this.expensesSum();
     return (
-      <div>Header</div>
+      <header>
+
+        <h3 data-testid="email-field">
+          Usuário:
+          {email}
+        </h3>
+
+        <p data-testid="total-field">
+          {totalExpenses
+            ? `Total de despesas: ${totalExpenses} BRL` : 'Total de despesas: 0'}
+        </p>
+
+        <p data-testid="header-currency-field">BRL</p>
+
+      </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  expenses: state.wallet.expenses,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
