328ea57eeb98222f4c8a94c543758d43f6af4c74
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-06-15 14:24:41 -0300

    req1 feito

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
-// initial commit
-
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+export default class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </Switch>
+    );
+  }
 }
-
-export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,74 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { setEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleClick = () => {
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(setEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const minPassword = 6;
+    const validEmail = emailRegex.test(email);
+    const validPassword = password.length >= minPassword;
+    const isValid = (validEmail && validPassword);
+    return (
+      <main>
+
+        <input
+          type="email"
+          data-testid="email-input"
+          onChange={ this.handleChange }
+          name="email"
+        />
+
+        <input
+          type="password"
+          data-testid="password-input"
+          onChange={ this.handleChange }
+          name="password"
+        />
+
+        <button
+          type="button"
+          disabled={ !isValid }
+          onClick={ this.handleClick }
+          data-testid="login-submit-button"
+        >
+          Entrar
+        </button>
+
+      </main>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  globalEmail: state.email,
+  globalPassword: state.password,
+});
+
+export default connect(mapStateToProps)(Login);
