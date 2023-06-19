d479ab0ca92baa6b666d6030ad22753f925cbad7
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-06-19 18:36:44 -0300

    req6 feito

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class Wallet extends React.Component {
           <Header />
           <WalletForm />
         </main>
+        <Table />
       </div>
     );
   }
